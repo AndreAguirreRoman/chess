dfe7c5f4b5dee0a8210f1782d77c30e8c5b21e11
AndreAguirreRoman <ankar_aguirre@live.com>
2025-02-26 21:21:26 -0700

    Added few changes

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,19 +1,20 @@
 package server;
 
 import spark.*;
+import spark.Response;
+import spark.Request;
+
+import java.util.UUID;
 
 public class Server {
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
-
         Spark.staticFiles.location("web");
-
         // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
-
+        // Spark.get("/game", this::getGames);
+        Spark.post("/user/register", this::createUser);
+        //This line initializes the server and can be removed once you have a functioning endpoint
         Spark.awaitInitialization();
         return Spark.port();
     }
@@ -22,4 +23,20 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private Object createUser(Request req, Response res){
+
+        return res;
+    }
+
+    private String createAuthToken(){
+        String authToken = UUID.randomUUID().toString();
+        return authToken;
+    }
+
+    /*private Object getGames(Request req, Response res) {
+
+    }*/
+
+
 }
