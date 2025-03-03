b6b3deff0cde0527d72f5c2bcc5bf20137b72719
AndreAguirreRoman <ankar_aguirre@live.com>
2025-03-02 22:09:39 -0700

    Finished the DAO working with the servicelayers now

--- server/src/main/java/dataaccess/DAO/AuthDao.java
+++ server/src/main/java/dataaccess/DAO/AuthDao.java
@@ -5,7 +5,7 @@ import model.AuthData;
 import model.UserData;
 
 public interface AuthDao {
-    AuthData createAuth(UserData userData) throws DataAccessException;
+    AuthData createAuth(UserData userData, String authToken) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/memory/MemoryAuthDao.java
+++ server/src/main/java/dataaccess/memory/MemoryAuthDao.java
@@ -1,5 +1,6 @@
 package dataaccess.memory;
 
+import dataaccess.DAO.AuthDao;
 import dataaccess.DataAccessException;
 import model.AuthData;
 import model.UserData;
@@ -7,21 +8,35 @@ import model.UserData;
 import java.util.HashMap;
 import java.util.UUID;
 
-public class MemoryAuthDao {
+public class MemoryAuthDao implements AuthDao {
     private final HashMap<String, AuthData> authTokens = new HashMap<>();
 
-    public AuthData createAuth(UserData userData) throws DataAccessException{
-        if (authTokens.containsKey(userData.id())){
-            throw new DataAccessException("Sus...");
+    public AuthData createAuth(UserData userData, String authToken) throws DataAccessException{
+        for (AuthData existingToken : authTokens.values()){
+            if (existingToken.userName().equals(userData.username())) {
+                return existingToken;
+            }
         }
         AuthData authData = new AuthData(generateToken(), userData.username());
         authTokens.put(authData.authToken(), authData);
         return authData;
     }
-    AuthData getAuth(String authToken) throws DataAccessException;
-    void deleteAuth(String authToken) throws DataAccessException;
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        if (!authTokens.containsKey(authToken)) {
+            throw new DataAccessException("No authorization!");
+        }
+        return authTokens.get(authToken);
+    }
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (!authTokens.containsKey(authToken)) {
+            throw new DataAccessException("No auth found!");
+        }
+        authTokens.remove(authToken);
+    }
 
     public static String generateToken() {
         return UUID.randomUUID().toString();
     }
+
+
 }
