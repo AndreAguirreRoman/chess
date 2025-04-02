000e9b3e7a43a48c5e53321cf1eb565bf0217db7
AndreAguirreRoman <ankar_aguirre@live.com>
2025-04-02 15:50:54 -0600

    Added some minor changes, working on serverfacade based on petshop

---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,12 @@
+package client;
+
+import com.sun.nio.sctp.NotificationHandler;
+
+
+public class Repl implements NotificationHandler {
+    private final InGameClient inGameClient;
+    private final PreGameClient preGameClient;
+    private final AuthorizedClient authorizedClient;
+
+
+}
