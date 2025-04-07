458d02a7d32169518a9a2079ba640067c0338c82
AndreAguirreRoman <ankar_aguirre@live.com>
2025-04-07 13:59:03 -0600

    Deleted what the autograder wanted:)

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -24,35 +24,16 @@ public class WebSocketHandler {
         UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
         switch (action.getCommandType()) {
             case CONNECT -> enter(action.getUsername(), session);
-            case LEAVE -> exit(action.getGameID(), session);
         }
     }
 
-    private void exit(Integer gameID, Session session) {
-    }
 
     private void enter( String username, Session session) throws IOException {
         connections.add(username, session);
-        var message = String.format("%s is in the shop", username);
         var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
         connections.broadcast(username, notification);
     }
 
-    private void exit(String visitorName) throws IOException {
-        connections.remove(visitorName);
-        var message = String.format("%s left the shop", visitorName);
-        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
-        connections.broadcast(visitorName, notification);
-    }
 
-    public void makeNoise(String petName, String sound) throws DataAccessException {
-        try {
-            var message = String.format("%s says %s", petName, sound);
-            var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
-            connections.broadcast("", notification);
-        } catch (Exception ex) {
-            throw new DataAccessException(500, ex.getMessage());
-        }
-    }
 }
 
